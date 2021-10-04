ae822a8b5717c54f3934394da02da15eeb889012
Kdiaseng <kalebdias.oficial@gmail.com>
2021-10-04 08:30:21 -0400

    feat: create list car

--- lib/pages/main_navigation/list_car/list_car_page.dart
+++ lib/pages/main_navigation/list_car/list_car_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:rentx/pages/main_navigation/list_car/widget/car_item_list_widget.dart';
+import 'package:rentx/pages/main_navigation/model/car_model.dart';
 import 'package:rentx/styles/app_colors.dart';
 
 class ListCarPage extends StatefulWidget {
@@ -12,42 +14,102 @@ class _ListCarPageState extends State<ListCarPage> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Stack(
-      children: [
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 25),
-          color: AppColors.black,
-          width: size.width,
-          height: size.height * 0.15,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Listagem',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
-                    fontWeight: FontWeight.w600),
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: size.height * 0.23,
+            child: Stack(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  color: AppColors.black,
+                  width: size.width,
+                  height: size.height * 0.2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Listagem',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        '4 carros',
+                        style: TextStyle(
+                            color: AppColors.grayText,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400),
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 16),
+                    height: 56,
+                    child: Row(
+                      children: [
+                        Container(
+                          color: AppColors.main,
+                          height: 56,
+                          width: size.width * 0.76,
+                          child: TextField(
+
+                            decoration: InputDecoration(
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 24),
+                              hintText: "Qual carro você procura?",
+                              focusedBorder: InputBorder.none,
+                              enabledBorder: InputBorder.none,
+                              errorBorder: InputBorder.none,
+                              disabledBorder: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        Container(color: Colors.white, width: 2),
+                        Expanded(
+                          child: Material(
+                            color: AppColors.main,
+                            child: InkWell(
+                              onTap: () {},
+                              child: Container(
+                                height: 56,
+                                child: Icon(Icons.search),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              shrinkWrap: true,
+              itemCount: 4,
+              itemBuilder: (context, index) => Column(
+                children: [
+                  CarItemListWidget(
+                    car: CarModel("Lamborghini", "Huracan", "images/lambo.png",
+                        "580", false),
+                  ),
+                  SizedBox(height: 14)
+                ],
               ),
-              Text(
-                '4 carros',
-                style: TextStyle(
-                    color: AppColors.grayText,
-                    fontSize: 13,
-                    fontWeight: FontWeight.w400),
-              )
-            ],
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: size.height * .8,
-            width: size.width,
-            color: Colors.green,
-          ),
-        )
-      ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/pages/main_navigation/list_car/widget/car_item_list_widget.dart
+++ lib/pages/main_navigation/list_car/widget/car_item_list_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:rentx/pages/main_navigation/model/car_model.dart';
 import 'package:rentx/styles/app_colors.dart';
 
@@ -15,16 +16,63 @@ class CarItemListWidget extends StatelessWidget {
         padding: const EdgeInsets.all(24),
         child: Row(
           children: [
-            Column(
-              children: [
-                Text(car.brand),
-                Text(car.model),
-                SizedBox(height: 16),
-                Text("AO DIA"),
-                Text(car.value),
-              ],
+            Container(
+              width: MediaQuery.of(context).size.width * .35,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    car.brand,
+                    style: TextStyle(
+                        color: AppColors.grayTextDetails,
+                        fontSize: 10,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    car.model,
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.grayTitle),
+                  ),
+                  SizedBox(height: 16),
+                  Text("AO DIA",
+                      style: TextStyle(
+                          color: AppColors.grayTextDetails,
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500)),
+                  SizedBox(height: 4),
+                  Row(
+                    children: [
+                      Text(
+                        'R\$ ${car.value}',
+                        style: TextStyle(
+                            color: AppColors.primary,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(width: 26),
+                      car.isGas
+                          ? SizedBox(
+                              height: 26,
+                              width: 20,
+                              child: SvgPicture.asset('images/gas.svg'))
+                          : SizedBox(
+                              height: 26,
+                              width: 20,
+                              child: SvgPicture.asset('images/eletric.svg')),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            Image.asset(car.image, fit: BoxFit.fill,)
+            Expanded(
+              child: Image.asset(
+                car.image,
+                fit: BoxFit.fill,
+              ),
+            )
           ],
         ),
       ),
